e1459714a99257dac377ca4f6d5350d4a50cb490
Charles <charlethical1@gmail.com>
2025-09-14 11:33:16 +0200

    Basic Bandit SSH connection

--- bandits/bandit_completer.py
+++ bandits/bandit_completer.py
@@ -1,8 +1,22 @@
 import paramiko
 
 
-def main():
-    print("hello")
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
 
+try:
+    client.connect("bandit.labs.overthewire.org", 2220, "bandit0", "bandit0")
+    stdin, stdout, stderr = client.exec_command("cat readme")
+    print("STDOUT:")
+    print(stdout.read().decode())
+    print("STDERR:")
+    print(stderr.read().decode())
 
-main()
+except paramiko.AuthenticationException:
+    print("Authentication failed.")
+except paramiko.SSHException as e:
+    print(f"SSH error: {e}")
+finally:
+    client.close()
+
+# regex for pass: \b[A-Za-z0-9]32\b
